2b6d609301f1ef0bd695614aca28b0ee497147e8
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2021-06-18 17:58:33 +0800

    implement ElGamal.py

---
+++ ElGamal.py
@@ -0,0 +1,125 @@
+from Crypto.Util.number import inverse as multiplicative_inverse
+from Crypto.Math.Primality import generate_probable_safe_prime
+from Crypto.Math.Numbers import Integer
+from Crypto.Random.random import randrange
+
+
+class ElGamal:
+    class PrivateKey:
+        def __init__(self, p=None, g=None, x=None):
+            self.p = p
+            self.g = g
+            self.x = x
+
+        def __repr__(self):
+            return f'ElGamal.PrivateKey(p={self.p}, g={self.g}, x={self.x})'
+
+        def __eq__(self, pk):
+            return self.p == pk.p and self.g == pk.g and self.x == pk.x
+
+    class PublicKey:
+        def __init__(self, p=None, g=None, h=None):
+            self.p = p
+            self.g = g
+            self.h = h
+
+        def __repr__(self):
+            return f'ElGamal.PublicKey(p={self.p}, g={self.g}, h={self.h})'
+
+        def __eq__(self, pk):
+            return self.p == pk.p and self.g == pk.g and self.h == pk.h
+
+    class Ciphertext:
+        def __init__(self, cm, cr, pk):
+            self.cm = cm # ciphertext with message
+            self.cr = cr # ciphertext with random number
+            self.pk = pk # the public key
+
+        def __neg__(self):
+            new_cm = multiplicative_inverse(self.cm, self.pk.p)
+            new_cr = multiplicative_inverse(self.cr, self.pk.p)
+            return ElGamal.Ciphertext(new_cm, new_cr, self.pk)
+
+        def __add__(self, e):
+            # homomorphic operation
+            assert self.pk == e.pk, 'The public keys should be the same!'
+            new_cm = self.cm * e.cm % self.pk.p
+            new_cr = self.cr * e.cr % self.pk.p
+            return ElGamal.Ciphertext(new_cm, new_cr, self.pk)
+
+        def __sub__(self, e):
+            return self + (-e)
+
+    @classmethod
+    def KeyGen(cls, nbits=512):
+        # generate prime
+        p = generate_probable_safe_prime(exact_bits=nbits)
+
+        # generate group generator
+        # reference: https://github.com/Legrandin/pycryptodome/blob/master/lib/Crypto/PublicKey/ElGamal.py#L62
+        while 1:
+            g = pow(Integer.random_range(min_inclusive=2, max_exclusive=p), 2, p)
+            if g in (1, 2):
+                continue
+            if (p - 1) % g == 0:
+                continue
+            ginv = g.inverse(p)
+            if (p - 1) % ginv == 0:
+                continue
+            break
+
+        p, g = int(p), int(g)
+
+        x = randrange(nbits+1, p)
+        h = pow(g, x, p)
+
+        return (ElGamal.PublicKey(p, g, h), ElGamal.PrivateKey(p, g, x))
+
+    @classmethod
+    def Encrypt(cls, pk, m, alpha=None):
+        if alpha is None:
+            alpha = randrange(0, pk.p-1)
+        cm = pow(pk.g, m, pk.p) * pow(pk.h, alpha, pk.p) % pk.p
+        cr = pow(pk.g, alpha, pk.p)
+        return ElGamal.Ciphertext(cm, cr, pk)
+
+    @classmethod
+    def Decrypt(cls, sk, c, message_space):
+        gm = c.cm * multiplicative_inverse(pow(c.cr, sk.x, sk.p), sk.p) % sk.p
+        for m in message_space:
+            if pow(sk.g, m, sk.p) == gm:
+                return m
+        raise ValueError('decryption failed')
+
+
+def test():
+    max_voter = 10
+    message_space = list(range(max_voter+1))
+    from random import choice
+
+    for round_ in range(5):
+        print(f'test round {round_} ... ', end='', flush=True)
+        nbits = 512
+        pk, sk = ElGamal.KeyGen(nbits)
+
+        # test decrypt
+        m = choice(message_space)
+        assert ElGamal.Decrypt(sk, ElGamal.Encrypt(pk, m), message_space) == m, 'test decryption failed'
+
+        # test homomorphic sub
+        while True:
+            m1 = choice(message_space)
+            m2 = choice(message_space)
+            if m1 - m2 in message_space:
+                break
+        alpha1 = randrange(pk.p - 1)
+        alpha2 = randrange(pk.p - 1)
+        c1 = ElGamal.Encrypt(pk, m1, alpha1)
+        c2 = ElGamal.Encrypt(pk, m2, alpha2)
+        assert ElGamal.Decrypt(sk, c1 - c2, message_space) == m1 - m2, 'test homomorphic add failed'
+
+        print('ok.')
+
+
+if __name__ == '__main__':
+    test()
--- SigmaProtocl.py
+++ SigmaProtocl.py
@@ -1,4 +1,4 @@
-from .Elgamal import ElGamal
+from .ElGamal import ElGamal
 from .Network import *
 import random
 
@@ -7,7 +7,7 @@ def Prover_0(e, alpha):
     alpha_ = random.randint(1, e.pk.p - 1)
     e_ = ElGamal.Encrypt(e.pk, 0, alpha_)
     send(e_)
-    
+
     c = recv()
     beta = c * alpha + alpha_
     send(beta)
@@ -18,10 +18,10 @@ def Verifier_0_get_conversation(e):
 
     c = random.randint(1, (e.pk.p - 1) // 2 - 1)
     send(c)
-    
+
     beta = recv()
     return (e, e_, c, beta)
-    
+
 def Verifier_0_verify(conversation):
     e, e_, c, beta = conversation
     return ElGamal.Encrypt(e.pk, 0, beta) == c * e + e_
@@ -37,21 +37,21 @@ def Simulator_0(e):
 def Prover_K(e, K, alpha):
     # note that the alpha here is the product of every alpha_k
     e_minus_K = ElGamal.Encrypt(e.pk, -K, 0)
-    
+
     Prover_0(e + e_minus_K, alpha)
-    
+
 def Verifier_K_get_conversation(e, K):
     e_minus_K = ElGamal.Encrypt(e.pk, -K, 0)
-    
+
     return Verifier_0_get_conversation(e + e_minus_K)
-    
+
 def Verifier_K_verify(conversation):
     return Verifier_0_verify(conversation)
 
 # Prove 01
 def Prover_01(e, m, alpha):
     assert e == ElGamal.Encrypt(e.pk, m, alpha)
-    
+
     # prepare
     b = m # stands for bit
     bb = 1 - m # stands for b bar
@@ -59,12 +59,12 @@ def Prover_01(e, m, alpha):
     c = [0, 0]
     beta = [0, 0]
     _, e_[bb], c[bb], beta[bb] = Simulator_0(e)
-    
-    # start the protocol
+
+    # start the underlying protocol
     #async Prover_K(e, m, alpha) # WTF why????
-    e_[b] = recv() 
+    e_[b] = recv()
     send(e_)
-    
+
     c_tmp = recv()
     c[b] = (c_tmp - c[bb]) % ((e.pk.p - 1) // 2)
     send(c[b])
@@ -73,13 +73,13 @@ def Prover_01(e, m, alpha):
 
 def Verifier_01_get_conversation(e):
     e_ = recv() # list of len = 2
-    
+
     c_tmp = random.randint(1, (e.pk.p - 1) // 2 - 1)
     send(c_tmp)
-    
+
     c, beta = recv() # both c and beta are list of len = 2
     return (e, e_, c_tmp, c, beta)
-    
+
 def Verifier_01_verify(conversation):
     e, e_, c_tmp, c, beta = conversation
     return (c_tmp == c[0] ^ c[1]) and \
